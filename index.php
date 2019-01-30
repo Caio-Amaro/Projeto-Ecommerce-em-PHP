a881daec9b5fdb55903b1cc0d2f6797ba7b2de3c
Caio Nunes <amaro.caio@gmail.com>
2019-01-30 17:20:42 -0200

    configurando template do administrador

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ use \Slim\Slim;
 
 use \Hcode\Page;
 
+use \Hcode\PageAdmin;
+
 $app = new Slim();
 
 $app->config('debug', true);
@@ -14,6 +16,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+	$page->setTpl("index");
+
+	
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 	

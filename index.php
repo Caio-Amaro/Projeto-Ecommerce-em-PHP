45dad723ca0cf8b8e269d677653a64414c4dc140
Caio Nunes <amaro.caio@gmail.com>
2019-02-26 13:12:29 -0300

    Finzalizando CRUD Categoria

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ session_start();
 require_once("vendor/autoload.php");
 
 
-
-
 use \Slim\Slim;
 
 use \Hcode\Page;
@@ -14,6 +12,9 @@ use \Hcode\Page;
 use \Hcode\PageAdmin;
 
 use \Hcode\Model\User;
+
+use \Hcode\Model\Category;
+
 
 $app = new Slim();
 
@@ -241,6 +242,103 @@ $app->post("/admin/forgot/reset", function(){
 		]);
 	
 		$page->setTpl("forgot-reset-success");
+});
+
+
+$app->get("/admin/categories", function(){
+
+	User::VerifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", array(
+
+		"categories"=>$categories
+	));
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::VerifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::VerifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("location: /admin/categories");
+
+	exit;
+	
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::VerifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("location: /admin/categories");
+
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory)
+{
+	User::VerifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+
+		"category"=>$category->getvalues()
+	]);
+
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory)
+{
+	User::VerifyLogin();
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setdata($_POST);
+
+	$category->save();
+
+	header("location: /admin/categories");
+
+	exit;
+
+	
+
 });
 
 

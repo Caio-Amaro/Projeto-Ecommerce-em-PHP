08c98b74faf3882f26105ce8b548011274978edc
Caio Nunes <amaro.caio@gmail.com>
2019-02-28 15:20:24 -0300

    Criação das categorias dinâmicas

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -34,6 +34,8 @@ class Category extends Model {
 
         $this->setData($resul[0]);
 
+        category::updateFile();
+
     }
 
 
@@ -63,6 +65,22 @@ class Category extends Model {
 
         ]);
 
+        category::updateFile();
+
+    }
+
+    public static function updateFile(){
+
+        $categories = category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+
+       file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
    
 
